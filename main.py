ffd732bda3331b4417ade3f27d293a31063f4528
SpawN 005 <jasserbouzidi22@gmail.com>
2024-05-01 11:26:21 +0200

    model tweaking

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException,Request
 from pydantic import BaseModel
 from typing import List
 import json
@@ -6,16 +6,11 @@ from matchmaking_algorithm import dynamic_grouping_based_on_performance
 
 app = FastAPI()
 
-class Team(BaseModel):
-    _id: str
-    win: int
-    lose: int
-    score: int
-
-
 @app.post("/group-teams")
-async def group_teams(teams: List, num_teams: int = 2):
+async def group_teams(request: Request, num_teams: int = 2):
+   
     try:
+        teams = await request.json()
         matched_teams = dynamic_grouping_based_on_performance(teams, num_teams)
         return matched_teams
     except Exception as e:
